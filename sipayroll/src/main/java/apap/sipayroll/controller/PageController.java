8038a311e612d6c19b82dd17e44a2b8c1f7752be
Raygue <45561907+Raygue@users.noreply.github.com>
2020-11-27 14:49:13 +0700

    Fitur1 (#2)
    * add user
    
    * add role
    
    * add page controller

---
+++ sipayroll/src/main/java/apap/sipayroll/controller/PageController.java
@@ -0,0 +1,32 @@
+package apap.sipayroll.controller;
+
+import apap.sipayroll.model.UserModel;
+import apap.sipayroll.service.RoleService;
+import apap.sipayroll.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
+
---
+++ sipayroll/src/main/java/apap/sipayroll/service/RoleService.java
@@ -0,0 +1,9 @@
+package apap.sipayroll.service;
+
+import apap.sipayroll.model.RoleModel;
+
+import java.util.List;
+
+public interface RoleService {
+    List<RoleModel> findAll();
+}
