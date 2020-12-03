534f244c3bb6968e41483c29efbf02c431ec1522
amilahzahiroh <amilahzahiroh@gmail.com>
2020-12-03 22:33:19 +0700

    adding view lembur, adjusting to revisi 1.1

--- sipayroll/src/main/java/apap/sipayroll/respository/LemburDb.java
+++ sipayroll/src/main/java/apap/sipayroll/respository/LemburDb.java
@@ -4,10 +4,12 @@ import apap.sipayroll.model.LemburModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LemburDb extends JpaRepository<LemburModel, Long> {
     Optional<LemburModel> findById(Long idLembur);
     void deleteById(Long idLembur);
+    List<LemburModel> findAll();
 }
